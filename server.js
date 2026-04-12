df66d468828929db5fb14e8d1c7292851b4b8bc1
rafear97115-create <rafear97115@gmail.com>
2026-04-12 16:31:01 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.json({ status: 'Agent Résidence de l\'Industrie opérationnel' });
